3962eb119e29bbe3227ada5aa2913679b18e4bc5
Damien Garaud <damien.garaud@gmail.com>
2016-04-01 15:08:21 +0200

    ended up week-cal

--- week-calendar.el
+++ week-calendar.el
@@ -4,6 +4,7 @@
 ;;   - https://www.emacswiki.org/emacs/InsertingTodaysDate
 ;;   - http://www.gnu.org/software/emacs/manual/html_node/elisp/Time-Calculations.html#Time-Calculations
 
+
 (require 'calendar)
 
 ;; pattern SDD DD==digit
@@ -11,17 +12,60 @@
   "pattern to match S{DD}")
 
 
-(string-match-p *pattern* "S12")
-(string-match-p *pattern* "12")
-(string-match *pattern* " s12 ") ;; curious: it's not case-sensitive
-(string-match *pattern* "W12")
-(string-match *pattern* "bla_S12_bla")
-
-
 (defun weekno (word)
   "extract the week number from a word such as S42"
   (if (eq 0 (string-match *pattern* word))
       (string-to-int (substring word 1))))
+
+
+(defun current-year ()
+  "return the current year"
+  ;; (string-to-int (format-time-string "%Y")))
+  (caddr (calendar-current-date)))
+
+
+(defun date-from-weekno (weekno)
+  "Get the date from a weekno. Return (month day year)."
+  (let ((year (current-year)))
+    (calendar-gregorian-from-absolute
+     (calendar-iso-to-absolute (list weekno 1 year)))))
+
+(defun swap-date (month day year)
+  ""
+  (list day month year))
+
+(defun str-date (date)
+  "string date format from a date (month day year)"
+  (format-time-string "%a %m %Y" (apply #'encode-time
+                                        (append '(0 0 0) (apply #'swap-date date)))))
+
+
+(defun insert-week-date ()
+  "Write a c++ output of the current word."
+  (interactive)
+  (setq cword (current-word))
+  (if (weekno cword)
+      (progn
+        (end-of-line 1)
+        (newline)
+        (beginning-of-line 1)
+        (insert (str-date (date-from-weekno (weekno cword)))))))
+
+
+(str-date (date-from-weekno (weekno "S42")))
+Mon 10 2016
+
+
+
+
+;; Some tests
+
+(string-match-p *pattern* "S12")
+(string-match-p *pattern* "12")
+(string-match *pattern* " s12 ") ;; curious: it's not case-sensitive
+(string-match *pattern* "W12")
+(string-match *pattern* "bla_S12_bla") ;; we don't want to match that...
+
 
 (substring "S12" 1)
 (weekno "S42")
@@ -30,18 +74,17 @@
 (format-time-string "%Y-%m-%d")
 (format-time-string "%Y-%U" (current-time))
 
-(defun current-year ()
-  "return the current year"
-  ;; (string-to-int (format-time-string "%Y")))
-  (caddr (calendar-current-date)))
 
-(defun date-from-weekno (weekno &optional year)
-  "Get the date from a weekno. Hanlde the year case"
-  (let ((year (current-year)))
-    (calendar-gregorian-from-absolute
-     (calendar-iso-to-absolute (list weekno 1 year)))))
+(append '(1 2 3) '("a" "b" ))
 
-(date-from-weekno 13)
+(apply #'encode-time (append '(0 0 0) '(12 3 2016)))
+(format-time-string "%Y-%m-%d" (encode-time 0 0 0 12 3 2016))
+(format-time-string "%a %m %Y" (encode-time 0 0 0 17 10 2016))
+(decode-time)
+(current-year)
+(current-time)
+
+((format-time-string "%Y-%U" (current-time))(date-from-weekno 13))
 
 ;; TODO
 ;; - encode-time from date-from-weekno
@@ -60,6 +103,7 @@
 (encode-time 0 0 0 17 10 2016)
 
 (calendar-gregorian-from-absolute (calendar-iso-to-absolute '(42 1 2016)))
+
 
 (calendar-iso-date-string (list 3 26 2016))
 (calendar-iso-read-date)
